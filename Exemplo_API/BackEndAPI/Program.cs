bb9cc698cd45ce8ac3cc25b9c7afeb17f53ff0d1
NataliaNogueira1 <natalianogueir2006@gmail.com>
2025-11-11 22:57:11 -0300

    Exemplo_API

---
+++ Exemplo_API/BackEndAPI/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+builder.Services.AddOpenApi();
+// Add Swagger Generator
+builder.Services.AddSwaggerGen();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
